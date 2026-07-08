e7e4d4afbc676f152c776c1e69b1c6114a27e913
Leapmotor ABRP Sync <test@example.com>
2026-07-08 07:15:18 +0000

    Fix UptimeRobot 501 Not Implemented (add HEAD handler)

--- render_app.py
+++ render_app.py
@@ -20,6 +20,11 @@ def run_sync():
         time.sleep(300)
 
 class DummyHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
